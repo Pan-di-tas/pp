69f05e2651c5b93f5d9ec82e7412b8a9009015e6
Abdiel <git config --global user.name Tu>
2025-09-04 10:40:57 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,17 +9,71 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _isPassword = false; // Para controlar la visibilidad de la contraseña
+
   @override
   Widget build(BuildContext context) {
+    // Obtener el tamaño de la pantalla del dispositivo
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      //Evita nidge o camaras
+      // Evita nudge o cámaras frontales para móviles
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: RiveAnimation.asset("assets/animated_login_character.riv"),
-            ),
-          ],
+        child: Padding(
+          // Eje x/horizontal/derecha izquierda
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset(
+                  "assets/animated_login_character.riv",
+                ),
+              ),
+              // Espacio entre el oso y el texto emaill
+              const SizedBox(height: 10),
+              // Campo de texto del email
+              TextField(
+                // Para que aparezca @ en móviles
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: const Icon(Icons.mail),
+                  border: OutlineInputBorder(
+                    // Esquinas redondeadas
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              // Campo de texto de password
+              const SizedBox(height: 10),
+              TextField(
+                // Para ocultar la contraseña
+                obscureText: !_isPassword, // Oculta la contraseña
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    // Esquinas redondeadas
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPassword
+                          ? Icons
+                                .visibility // Ojo cerrado
+                          : Icons.visibility_off, // Ojo abierto
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPassword = !_isPassword;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
